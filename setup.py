a169ff0e7d0a9b79b8477e607e3c3d0288fefbe3
StarNumber <francesco.lumaca2009@gmail.com>
2022-02-15 18:21:15 +0100

    fixed with commit

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
 import os
 import platform
 from setuptools import setup
+import re, subprocess
 req = """
 requests
 aiohttp
 """
 
-requirements = []
+with open('baracchino/__init__.py') as f:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
+if not version:
+    raise RuntimeError('version is not set')
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+
+        p = os.popen(" ".join(['git', 'describe', '--always']))
+        version += p.read()[:-1].replace('a', "").replace('b', "").replace('c', "").replace('d', "").replace('e', "").replace('f', "").replace('g', "").replace('h', "").replace("i", "")
+        print(version)
+    except Exception:
+        import traceback
+        traceback.print_exc()
+        print(Exception())
+requirements = []
+print(f"Using version {version}")
 requirements = req.splitlines()
-setup(name="baracchino.py", version="1.0.4beta", packages=["baracchino"], license="MIT", install_requires=requirements, classifiers=['Development Status :: 5 - Production/Stable',
+setup(name="baracchino.py", version=f"{version}", packages=["baracchino"], license="MIT", install_requires=requirements, classifiers=['Development Status :: 5 - Production/Stable',
                                                                                                                           'License :: OSI Approved :: MIT License',
                                                                                                                           'Intended Audience :: Developers',
                                                                                                                           'Natural Language :: English',
